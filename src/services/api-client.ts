f2d1ea8ace18d9a4333cad5073e9e51d280812ed
siavash sattari <siavash.sattari.dev@gmail.com>
2023-04-21 22:07:16 +0330

    implement platform icons

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: 'c7b18323a47d40c394ea5b019646b1f5'
+    key: '36ff906a9100474791ce6c4a2b6ce642'
   }
 });
